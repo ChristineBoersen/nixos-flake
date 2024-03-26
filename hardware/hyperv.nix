e2127a6d18472295bd7f6e99ccca8318486ee69a
Christine Boersen <13676509+ChristineBoersen@users.noreply.github.com>
2024-03-26 19:36:19 -0400

    bugs

--- hardware/hyperv.nix
+++ hardware/hyperv.nix
@@ -4,17 +4,15 @@
 {
   imports = [];
 
-  lib.mkIf (virtualisation.hypervGuest.enable == true) {
+  virtualisation.hypervGuest.videoMode = mkDefault "1600x900"
 
-      virtualisation.hypervGuest.videoMode = mkDefault "1600x900"
+  boot.kernelParams = ["video=hyperv_fb:1600x900"];  # https://askubuntu.com/a/399960
+  boot.kernel.sysctl."vm.overcommit_memory" = "1"; # https://github.com/NixOS/nix/issues/421;
 
-      boot.kernelParams = ["video=hyperv_fb:1600x900"];  # https://askubuntu.com/a/399960
-      boot.kernel.sysctl."vm.overcommit_memory" = "1"; # https://github.com/NixOS/nix/issues/421;
+  environment = {
+    systemPackages  = (with pkgs; []);
+  };
+  nixpkgs.hostPlatform = lib.mkForce "x86_64-linux";
 
-      environment = {
-        systemPackages  = (with pkgs; []);
-      };
-      nixpkgs.hostPlatform = lib.mkForce "x86_64-linux";
-   };
 
 }
