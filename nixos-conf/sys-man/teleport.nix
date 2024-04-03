4a68b48ec7c3f1267337ed82caaae4787865b602
Christine Boersen <13676509+ChristineBoersen@users.noreply.github.com>
2024-04-03 11:34:38 -0400

    teleport

---
+++ nixos-conf/sys-man/teleport.nix
@@ -0,0 +1,25 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running nixos-help).
+
+{ config, lib, pkgs, options, ... }:
+
+{
+  networking.firewall.allowedTCPPorts = [  ];
+  environment = {
+  # environment.etc
+    etc = {
+      # use this section to insert items into the etc dir. The keyname is the filename without the /etc/ prepended to the path
+
+    };
+
+    systemPackages = (with pkgs; [
+     teleport
+
+
+    ]);
+
+  };
+
+
+}
