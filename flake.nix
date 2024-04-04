e6f2a942b70720908d3e2709df2f1923dad5419a
Christine Boersen <13676509+ChristineBoersen@users.noreply.github.com>
2024-04-04 02:00:26 -0400

    ddddd

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     inputs = {
       nixpkgs = {
         #url = "github:NixOS/nixpkgs/staging-next";
-        url = "github:NixOS/nixpkgs?ref=nixpkgs-unstable#e976fa8f49c3";
+        url = "github:NixOS/nixpkgs?ref=nixpkgs-unstable";
       };
 
       nix-darwin = {
