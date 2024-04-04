750956ac8299ccbe632de11a9b06fb9fef70e6a7
Christine Boersen <13676509+ChristineBoersen@users.noreply.github.com>
2024-04-04 01:11:07 -0400

    g

--- nixos-hosts/mgmt.mclsystems.com/ipa1/configuration.nix
+++ nixos-hosts/mgmt.mclsystems.com/ipa1/configuration.nix
@@ -10,7 +10,7 @@
       ./hardware-configuration.nix
       ../../../hardware/hyperv.nix
       ../../../nixos-conf/desk-env/kde_plasma6.nix
-      ../../../nixos-conf/sys-man/freeipa.nix
+      #../../../nixos-conf/sys-man/freeipa.nix
     ];
 
   # Enable networking
