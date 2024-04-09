8cf3f4a7e5d8099c413d6cc66057061277084043
Christine Boersen <13676509+ChristineBoersen@users.noreply.github.com>
2024-04-09 12:26:02 -0400

    freeipa is a bust (389 DS has been broken for 2 years on nix) Re-trying step-ca

---
+++ nixos-conf/sys-man/stepca.nix
@@ -0,0 +1,49 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running nixos-help).
+
+{ config, lib, pkgs, options, ... }:
+
+{
+  networking.firewall.allowedTCPPorts = [  ];
+
+  # broken package
+  #nixpkgs.config.allowBroken = true;
+
+  environment = {
+  # environment.etc
+    etc = {
+      # use this section to insert items into the etc dir. The keyname is the filename without the /etc/ prepended to the path
+
+    };
+
+    systemPackages = (with pkgs; [
+     #step-ca
+     #step-cli
+
+
+    ]);
+
+  };
+
+  services.step-ca = {
+    enable = lib.mkDefault true;
+    address = "10.240.0.8";
+    port = 8443;
+  };
+
+#   security.ipa = {
+#     enable = lib.mkDefault true;
+#     domain = "${config.networking.domain}";
+#     realm = "${lib.strings.toUpper config.networking.domain}";
+#     server = "${config.networking.fqdn}";
+#     basedn = lib.concatStringsSep ", "  (lib.forEach (lib.splitString "." "${config.networking.domain}") (name: "DC=${name}")); # convert domain to distinguished name
+#     #certificate = builtins.readFile /run/keys/root_ca.crt;
+#     certificate = pkgs.fetchurl {
+#       url = "http://${config.networking.fqdn}/config/ca.crt";  # "file://run/keys/root_ca.crt";  #
+#       sha256 = "";
+#     };
+#   };
+  
+
+}
